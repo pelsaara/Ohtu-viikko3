200260faf063d53c315ce46761190a192270aeaf
pelsaara <saara.peltomaki@gmail.com>
2017-11-19 23:21:05 +0200

    tehtava 7

--- LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -39,8 +39,14 @@ public class AuthenticationService {
     }
 
     private boolean invalid(String username, String password) {
-        // validity check of username and password
+        if (username.length() >= 3 && username.matches("[a-zA-Z]*") && password.length() >= 8) {
+            for (Character c : password.toCharArray()) {
+                if (Character.isDigit(c) || (!Character.isAlphabetic(c) && !Character.isISOControl(c))) {
+                    return false;
+                }
+            }
 
-        return false;
+        }
+        return true;
     }
 }
