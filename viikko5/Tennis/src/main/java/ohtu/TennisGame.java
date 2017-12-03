cd4b967248d0cadd854cf70e159107474f0723bc
pelsaara <saara.peltomaki@gmail.com>
2017-12-03 20:31:14 +0200

    tehtava 2

--- viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,9 +1,9 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int scoreP1 = 0;
+    private int scoreP2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,67 +13,74 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            scoreP1++;
+        } else {
+            scoreP2++;
+        }
+
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (scoreP1 == scoreP2) {
+            score = equalPoints(score);
+        } else if (scoreP1 >= 4 || scoreP2 >= 4) {
+            score = winningPlayer(score);
+        } else {
+            score = currentGameStatus(score);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return score;
+    }
+
+    public String equalPoints(String score) {
+        score = getResult(score, scoreP1);
+        if (!score.equals("Deuce")) {
+            score += "-All";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return score;
+    }
+
+    public String winningPlayer(String score) {
+        int minusResult = scoreP1 - scoreP2;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
+    public String currentGameStatus(String score) {
+
+        score += getResult(score, scoreP1);
+        score += "-";
+        score += getResult(score, scoreP2);
+
+        return score;
+    }
+
+    public String getResult(String score, int playerScore) {
+        switch (playerScore) {
+            case 0:
+                score = "Love";
+                break;
+            case 1:
+                score = "Fifteen";
+                break;
+            case 2:
+                score = "Thirty";
+                break;
+            case 3:
+                score = "Forty";
+                break;
+            default:
+                score = "Deuce";
+                break;
         }
         return score;
     }
